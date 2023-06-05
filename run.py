b6b11c3b41468101cfe327811d72106b7a07932b
danny-fla <dannyfla@live.ie>
2023-06-05 17:11:45 +0000

    add green option to wheel. add bet mapping to ensure the bet choice correctly represents its value. loop through bet mapping to check if win matches bet choice. remove old check winnings function and use modified version. wrap main funciton inside while loop so the user can play mulitple times

--- run.py
+++ run.py
@@ -5,11 +5,12 @@ MAX_BET_SELECTION = 10
 MIN_BET_AMOUNT = 1
 MAX_BET_AMOUNT = 5000
 
-wheel_numbers = [32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30,
+wheel_numbers = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30,
                  8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29,
                  7, 28, 12, 35, 3, 26]
 
 number_colors = {
+    0: 'Green',
     32: 'Red',
     19: 'Red',
     21: 'Red',
@@ -57,31 +58,39 @@ def add_money():
                 break
             else:
                 print("Minimum deposit is €1.")
-        
+       
         except ValueError:
             print("Please enter a valid number.")
 
     return amount
 
-
 def get_bet_choice():
+    bet_mapping = {
+        1: 'Red',
+        2: 'Black',
+        3: 'Even',
+        4: 'Odd',
+        5: 'Green'
+    }
+
     print("Type '1' for Red.")
     print("Type '2' for Black.")
     print("Type '3' for Even.")
     print("Type '4' for Odd.")
-    # print("Type '5' for Green.")
-    # print("Type '6' for Straight.")
+    print("Type '5' for Green.")
 
     while True:
         bet_choice = input("Enter the number corresponding to your desired bet: ")
         if bet_choice.isdigit():
             bet_choice = int(bet_choice)
-            if 1 <= bet_choice <= 4:
-                return bet_choice
+            if bet_choice in bet_mapping:
+                return bet_mapping[bet_choice]
             else:
-                print("Error: Invalid bet choice. Please enter a number from 1 to 6.")
+                print("Error: Invalid bet choice. Please enter a number from 1 to 5.")
         else:
             print("Error: Invalid input. Please enter a number.")
+
+
 
 def get_bet_amount(choice):
     while True:
@@ -103,15 +112,53 @@ def spin_roulette_wheel():
     winning_color = number_colors[winning_number]
     return winning_color, winning_number
 
-# Example usage
-balance = add_money()
-print(f"Your balance is €{balance}")
-choice = get_bet_choice()
-print("You chose:", choice)
-stake = get_bet_amount(choice)
-print("Your stake is: €", stake)
-winning_color, winning_number = spin_roulette_wheel()
-print("The colour is:", winning_color)
-print("The number is:", winning_number)
+
+def check_winnings(winning_color, winning_number, choice, stake):
+    winnings = 0
+    if winning_color == choice:
+        print("Checking winning color")
+        if winning_color == 'Red' or winning_color == 'Black':
+            winnings = int(stake) * 2
+        elif winning_color == 'Green':
+            winnings = int(stake) * 15
+    elif winning_number % 2 != 0 and choice == 'Odd':
+        print("Checking if odd")
+        winnings = int(stake) * 2
+    elif winning_number % 2 == 0 and choice == 'Even':
+        print("Checking if even")
+        winnings = int(stake) * 2
+
+    return winnings
 
 
+while True:
+    # Initial balance
+    balance = add_money()
+    print(f"Your balance is €{balance}")
+
+    while True:
+        # Get bet choice and amount
+        choice = get_bet_choice()
+        stake = get_bet_amount(choice)
+
+        # Spin the wheel and determine the outcome
+        winning_color, winning_number = spin_roulette_wheel()
+        winnings = check_winnings(winning_color, winning_number, choice, stake)
+
+        # Display the outcome and update the balance
+        print(f"The ball has landed on {winning_color} {winning_number}")
+        print(f"You bet €{stake}, and you won €{winnings}!")
+        balance += winnings
+        print(f"Your updated balance is €{balance}")
+
+        # Check if the user wants to play again
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != "yes":
+            break
+
+    # Check if the user wants to continue playing
+    continue_playing = input("Do you want to leave the table? (yes/no): ")
+    if continue_playing.lower() != "no":
+        break
+
+print('Your closing balance is: ', balance)
